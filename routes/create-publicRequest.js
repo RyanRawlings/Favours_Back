b18ab86443af8d6f83c907fef2cf183d3090caaa
Ryan Rawlings <ryan.t.rawlings@student.uts.edu.au>
2020-10-14 11:59:12 +1100

    Create public requests reward object array

--- routes/create-publicRequest.js
+++ routes/create-publicRequest.js
@@ -1,28 +1,43 @@
 const PublicRequest = require("../models/PublicRequest");
 const User = require("../models/User");
-const router = require('express').Router();
+const router = require("express").Router();
+const mongoose = require("mongoose");
 
 router.post("/create-publicRequest", async (req, res) => {
-
     console.log("Create public request started...");
-
     // console.log(req.body);
+    // console.log(req.body.rewards);
+
+    // Get the document relating to the requesting user
     const user = await User.findOne({ email: req.body.requestedBy });
-    // const favourType = await FavourType.findOne({ name: req.body.rewards.re });
+    
+    // Store UserId in separate variable from retrieved document
     const requestUser = user._id;
 
-    //Create Public Request
+    // Create Public Request
     const publicRequest = new PublicRequest({
       requestUser: requestUser,
       title: req.body.requestTitle,
       description: req.body.requestTaskDescription,
-      rewards: [{
-                    item: req.body.rewards.rewardId,
-                    quantity: req.body.rewards.rewardQuantity,
-                    providedBy: req.body.rewards.offeredById
-      }],
-    completed: false,
+      rewards: [],
+      completed: false,
     });
+    
+    let rewardsArray = [];
+    for (let i = 0; i < req.body.rewards.length; i++) {
+      console.log("Post creation child push start...");
+      // console.log(req.body.rewards[0].rewardId);
+      // console.log(req.body.rewards[0].offeredById);
+      
+      rewardsArray.push({
+            item: mongoose.Types.ObjectId(req.body.rewards[i].rewardId),
+            quantity: req.body.rewards[i].rewardQuantity,
+            providedBy: mongoose.Types.ObjectId(req.body.rewards[i].offeredById)
+      })
+
+      publicRequest.rewards.push(rewardsArray[i]);
+    }  
+
     try {
       const savedPublicRequest = await publicRequest.save();
       res.send({ publicRequestId: savedPublicRequest._id });
