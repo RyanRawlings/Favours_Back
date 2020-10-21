eaafcc88ec1a83a90a7377430bff572f7f6dcce8
Arsedian Ivanurrahman <arsedianivanurrahman@192-168-1-104.tpgi.com.au>
2020-10-21 22:45:53 +1100

    Setup EC2 Build

---
+++ src/build/precache-manifest.350561c9b40bc906ae553096bc0e0508.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a277d7c71db024e1fafbe28a4da47022",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c3aeeb9e92f6e781c76",
+    "url": "/static/css/main.0045e5ef.chunk.css"
+  },
+  {
+    "revision": "0c207ea6efe6276912c7",
+    "url": "/static/js/2.7efe2d83.chunk.js"
+  },
+  {
+    "revision": "1fbf4f729e95c9203118591f90576fb0",
+    "url": "/static/js/2.7efe2d83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c3aeeb9e92f6e781c76",
+    "url": "/static/js/main.e8b22273.chunk.js"
+  },
+  {
+    "revision": "59d58e7edb60f10a4af5",
+    "url": "/static/js/runtime-main.25801cac.js"
+  },
+  {
+    "revision": "feb71cfbad3d355027ee6605da13573d",
+    "url": "/static/media/hero-image-people-cartoon-tb.feb71cfb.png"
+  },
+  {
+    "revision": "df66f48c4cc77660653cf3abb778ed26",
+    "url": "/static/media/uts-hero-image.df66f48c.png"
+  }
+]);
